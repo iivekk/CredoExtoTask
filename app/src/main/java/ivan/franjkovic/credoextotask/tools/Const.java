efeed4903e9279addb5de3f823bf56ad57d262f9
iivekk <iwan.franjkovic@gmail.com>
2019-12-31 14:48:45 +0100

    line drawing inserted

---
+++ app/src/main/java/ivan/franjkovic/credoextotask/tools/Const.java
@@ -0,0 +1,11 @@
+package ivan.franjkovic.credoextotask.tools;
+
+public interface Const {
+
+    int NEW_ELEMENT_REQUEST_CODE = 1;
+    String INTENT_ACTION_DRAW = "INTENT_ACTION_DRAW";
+    String LIST_OF_SORTED_DATA_ID = "list_sorted_data_id";
+    String ITEM_HEIGHT = "item_height";
+    String PREFERENCE_FILE = "preference_file";
+
+}
